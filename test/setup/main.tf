286e2af9920f7253ef3986a93110e4f5fa89ec22
Fellipe Medeiros <femedeiros@google.com>
2024-04-09 14:08:09 -0300

    Create main.tf

---
+++ test/setup/main.tf
@@ -0,0 +1,44 @@
+/**
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+module "project" {
+  source  = "terraform-google-modules/project-factory/google"
+  version = "~> 14.0"
+
+  name              = "terraform-dataanalytics-eventdriven"
+  random_project_id = "true"
+  org_id            = var.org_id
+  folder_id         = var.folder_id
+  billing_account   = var.billing_account
+
+  activate_apis = [
+   "cloudbuild.googleapis.com",
+   "bigquery.googleapis.com",
+   "cloudresourcemanager.googleapis.com",
+   "cloudfunctions.googleapis.com",
+   "compute.googleapis.com",
+   "eventarc.googleapis.com",
+   "iam.googleapis.com",
+   "run.googleapis.com",
+   "pubsub.googleapis.com",
+   "secretmanager.googleapis.com",
+   "servicenetworking.googleapis.com",
+   "storage.googleapis.com",
+   "storage-component.googleapis.com",
+   "eventarc.googleapis.com",
+   "eventarcpublishing.googleapis.com"
+  ]
+}
