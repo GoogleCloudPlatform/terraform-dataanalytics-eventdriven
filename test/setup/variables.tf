5f38b2b266edec77ab07fcb30b219025eae3b746
Fellipe Medeiros <femedeiros@google.com>
2024-04-10 17:35:28 -0300

    Update variables.tf

--- test/setup/variables.tf
+++ test/setup/variables.tf
@@ -36,6 +36,14 @@ variable "resource_labels" {
   default     = {}
 }
 
+variable "org_id" {
+  description = "The numeric organization id"
+}
+
+variable "folder_id" {
+  description = "The folder to deploy in"
+}
+
 variable "billing_account" {
   description = "The billing account id associated with the project, e.g. XXXXXX-YYYYYY-ZZZZZZ"
 }
