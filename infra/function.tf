479de5a2d4c34865a4198daa66333983337a5195
Fellipe Medeiros <femedeiros@google.com>
2024-06-03 21:59:24 +0000

    create event arc sa

--- infra/function.tf
+++ infra/function.tf
@@ -86,7 +86,7 @@ resource "google_cloud_run_service_iam_member" "run_service_member" {
 
 # gate resource creation until APIs are enabled, using approximate timeout
 # if terraform reports an error, run "apply" again
-resource "time_sleep" "wait_for_apis" {
+/*resource "time_sleep" "wait_for_apis" {
   depends_on = [
     google_project_iam_member.gcs_to_pubsub,
     google_project_iam_member.event_receiver,
@@ -94,4 +94,31 @@ resource "time_sleep" "wait_for_apis" {
   ]
 
   create_duration = var.time_to_enable_apis
+}*/
+
+# Set up Eventarc service account for the Cloud Function to execute as
+# # Set up the Eventarc service account
+resource "google_service_account" "eventarc_service_account" {
+  project      = module.project-services.project_id
+  account_id   = "eventarc-sa-${random_id.id.hex}"
+  display_name = "Service Account for Cloud Eventarc"
+}
+
+# # Grant the Eventarc service account Workflow Invoker Access
+resource "google_project_iam_member" "eventarc_service_account_invoke_role" {
+  project = module.project-services.project_id
+  role    = "roles/workflows.invoker"
+  member  = "serviceAccount:${google_service_account.eventarc_service_account.email}"
+
+  depends_on = [
+    google_service_account.eventarc_service_account
+  ]
+}
+
+# # Get the Pub/Sub service account to trigger the pub/sub notification
+# # TODO: File bug for this to be a pickable service account
+resource "google_project_iam_member" "pub_sub_permissions_token" {
+  project = module.project-services.project_id
+  role    = "roles/iam.serviceAccountTokenCreator"
+  member  = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-pubsub.iam.gserviceaccount.com"
 }
