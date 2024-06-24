454ad0c9034c7fc84c63ae9501fb238926e2ad47
Fellipe Medeiros <femedeiros@google.com>
2024-06-24 22:32:18 +0000

    add versions and bq

--- main.tf
+++ main.tf
@@ -185,3 +185,50 @@ resource "google_project_service_identity" "eventarc_agent" {
   project  = module.project_services.project_id
   service  = "eventarc.googleapis.com"
 }
+
+resource "google_bigquery_dataset" "ecommerce" {
+  dataset_id  = "ecommerce"
+  description = "Store ecommerce data"
+  location    = var.region
+  labels      = local.resource_labels
+}
+
+resource "google_bigquery_table" "order_events" {
+  dataset_id          = google_bigquery_dataset.ecommerce.dataset_id
+  table_id            = "order_events"
+  description         = "Store order events"
+  deletion_protection = false
+
+  time_partitioning {
+    type  = "DAY"
+    field = "action_time"
+  }
+
+  labels = local.resource_labels
+
+  schema = <<EOF
+[
+  {
+    "name": "order_id",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "customer_email",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "action",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "action_time",
+    "type": "TIMESTAMP",
+    "mode": "NULLABLE"
+  }
+]
+EOF
+
+}
